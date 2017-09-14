73440fe0fe79a645f5c80e7fd6fa6ad3d16c0b39
adwiputra <a.dwiputra@cgiar.org>
2017-09-14 16:33:48 +0700

    deletion of unneccessary files

--- alfa.rsx
+++
@@ -1,3 +0,0 @@
-library(raster)
-r<-raster(nrows=108, ncols=21, xmn=0, xmx=10)
-writeRaster(r)
--- arizka_push.R
+++
@@ -1,9 +0,0 @@
-library(abind)
-
-a <- 2
-b <- a+2
-
-print("A Mufida sedang makan")
-for(i in 1: 1000){
-  print(paste0(i, "piring mie"))
-}
